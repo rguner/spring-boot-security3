ecaa3de5e31ddfa5310c778f31dce5a9a85ff32c
Ramazan Guner <ramazan.guner@haud.com>
2023-12-21 20:15:53 +0300

    first commit

---
+++ src/main/java/com/guner/springbootsecurity3/SpringBootSecurityApplication3.java
@@ -0,0 +1,13 @@
+package com.guner.springbootsecurity3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityApplication3 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityApplication3.class, args);
+    }
+
+}
---
+++ src/test/java/com/guner/springbootsecurity3/SpringBootSecurityApplication3Tests.java
@@ -0,0 +1,13 @@
+package com.guner.springbootsecurity3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplication2Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
